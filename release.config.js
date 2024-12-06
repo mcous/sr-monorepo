4d136e46a1c2c450529d10ac51e4778cdebd5c2c
Michael Cousins <michael.cousins@viam.com>
2024-12-06 10:36:36 -0500

    chore: get baseline working

--- release.config.js
+++ release.config.js
@@ -1,4 +1,8 @@
 export default {
   preset: 'conventionalcommits',
   branches: ['main', { name: 'next', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+  ],
 };
